fb11e3225364b68e36c86ac5d2f4813c86aa9f2b
ilteelte <teomitioglo@gmail.com>
2019-11-23 18:31:53 +0100

    Step 3. Fetching quotes from the API

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,8 +1,18 @@
 import React, { Component } from "react";
+import QuoteSearcher from "./QuoteSearcher";
 
 class Quote extends Component {
   render() {
-    return <div></div>;
+    console.log("Heloo from Quote: ", this.text);
+
+    // console.log("this.quote_text", this.props);
+
+    return (
+      <div>
+        {this.props.text}
+        <p>by:{this.props.author}</p>
+      </div>
+    );
   }
 }
 
